cdf849f8aa6fe83d01c3a4b08ad1dbae0ae4155d
Arjun Rao <6134623+arjun-rao@users.noreply.github.com>
2020-03-15 23:28:01 -0600

    Create custom.ts

---
+++ custom.ts
@@ -0,0 +1,42 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+enum ConfigModes {
+    //% block="one"
+    None,
+    Select,
+    //% block="two"
+    Gestation,
+    Contagiousness,
+    Infection
+}
+
+enum Contagiousness {
+    //% block="one"
+    None,
+    low,
+    //% block="two"
+    medium,
+    high,
+}
+
+
+enum GestationPeriods {
+    //% block="one"
+    None,
+    TwoSeconds,
+    //% block="two"
+    FiveSeconds,
+    TenSeconds,
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+   
+}
